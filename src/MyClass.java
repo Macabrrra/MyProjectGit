7c879c7ec2b570748f1b8afb804690679694b6fb
Wojciech Kotlowski <wojciech.kotlowski@sap.com>
2017-04-10 18:49:29 +0200

    MySecondCommit

---
+++ src/MyClass.java
@@ -0,0 +1,18 @@
+
+public class MyClass {
+	private int a;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public MyClass(int a) {
+		this.a = a;
+	}
+	
+	
+}
